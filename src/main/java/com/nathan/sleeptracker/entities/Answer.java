753ff0d6d77c15838eb3cd27da4c645b21e1cb8a
Nathanknr <nathankamgang4270@gmail.com>
2025-05-29 20:39:57 +0100

    feat: create the Answer entity

---
+++ src/main/java/com/nathan/sleeptracker/entities/Answer.java
@@ -0,0 +1,4 @@
+package com.nathan.sleeptracker.entities;
+
+public class Answer {
+}
---
+++ src/main/java/com/nathan/sleeptracker/services/AnswerService.java
@@ -0,0 +1,8 @@
+package com.nathan.sleeptracker.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class AnswerService {
+
+}
